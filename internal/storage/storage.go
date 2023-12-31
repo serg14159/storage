2956ee480badff9a17bfbc8342ac826fead51bf7
serg14159 <serg301@yandex.ru>
2023-12-31 20:18:15 +0300

    Add simple upload method

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,21 @@
 package storage
 
+import (
+	"github.com/serg14159/storage/internal/file"
+)
+
 type Storage struct{}
 
 func NewStorage() *Storage {
 	return &Storage{}
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	return file.NewFile(filename, blob)
+	//newFile, err := file.NewFile(filename, blob)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// return newFile
 }
